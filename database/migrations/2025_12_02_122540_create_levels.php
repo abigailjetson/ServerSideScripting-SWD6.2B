ee069f88d377c9014aa1ed26b16ae3b92dfb3e89
abigailjetson <abigailjetson@gmail.com>
2025-12-02 12:41:21 +0000

    changed models

--- database/migrations/2025_12_02_122540_create_levels.php
+++ database/migrations/2025_12_02_122540_create_levels.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('lvl_model', function (Blueprint $table) {
+        Schema::create('levels', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('lvl_model');
+        Schema::dropIfExists('levels');
     }
 };
