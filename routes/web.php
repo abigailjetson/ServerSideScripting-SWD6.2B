36aab3d4a7b02f72e58292a4c6980f17c360f5bb
abigailjetson <abigailjetson@gmail.com>
2025-12-10 13:22:58 +0000

    web.php updated

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,14 @@ Route::resource('categories', CategoryController::class);
 Route::resource('cuisines', CuisineController::class);
 Route::resource('recipes', RecipeController::class);
 
+Route::get('/all-recipes', function () {
+    return "<h1>All Recipes</h1>";
+});
+
+Route::get('/all-cuisines', function () {
+    return "<h1>All Cuisines</h1>";
+});
+
+Route::get('/all-categories', function () {
+    return "<h1>All Categories</h1>";
+});
