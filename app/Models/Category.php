9e5b8ac76c5aa4fd5de729e08ed0c247b3f226cd
abigailjetson <abigailjetson@gmail.com>
2025-12-22 11:39:48 +0000

    Added migrations and models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,10 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['name']; // breakfast, lunch, dinner
-
-    public function recipes()
-    {
-        return $this->hasMany(Recipe::class);
-    }
+    //
 }
--- app/Models/Cuisine.php
+++ app/Models/Cuisine.php
@@ -6,10 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cuisine extends Model
 {
-    protected $fillable = ['name'];
-
-    public function recipes()
-    {
-        return $this->hasMany(Recipe::class);
-    }
+    //
 }
